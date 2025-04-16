41f5dabd146aa1d04862b69337c152c8a07828d3
Felipe Lima Santos <felipeex@MacBook-Air-de-Felipe.local>
2025-04-15 22:59:31 -0300

    .

--- exercises/list-four-vetor/segundo-exercicio.java
+++ exercises/list-four-vetor/segundo-exercicio.java
@@ -1,4 +1,16 @@
+import java.util.Arrays;
+
 class SegundoExercicio {
+ static int[][] fakeQM(int n) {
+    int matriz[][] = new int[n][n];
+
+    for(int linha = 0; linha < n; linha++)
+      for(int coluna = 0; coluna < n; coluna++)
+        matriz[linha][coluna] = 1;
+
+    return matriz;
+  }
+
   static int[][] gerarMatrizQuadrada(int n) {
     int matriz[][] = new int[n][n];
 
@@ -14,8 +26,29 @@ class SegundoExercicio {
       for(int coluna = 0; coluna < matriz.length; coluna++)
         System.out.println(matriz[linha][coluna]);
   }
+
+  static void verificaQM(int matriz[][]) {
+    int somaElementosLinha[] = new int[matriz.length], somaElementosColuna[] = new int[matriz.length];
+
+    for(int linha = 0; linha < matriz.length; linha++) {
+      for(int coluna = 0; coluna < matriz.length; coluna++) {
+        somaElementosLinha[linha] += matriz[linha][coluna];
+      }
+    }
+
+    for(int coluna = 0; coluna < matriz.length; coluna++) {
+      for(int linha = 0; linha < matriz.length; linha++) {
+        somaElementosColuna[coluna] += matriz[linha][coluna];
+      }
+    }
+
+    System.out.println(Arrays.toString(somaElementosLinha));
+    System.out.println(Arrays.toString(somaElementosColuna));
+  }
   
   public static void main(String[] args) {
-    exibeMatrizQuadrada(gerarMatrizQuadrada(10));
+    // exibeMatrizQuadrada(gerarMatrizQuadrada(10));
+    verificaQM(gerarMatrizQuadrada(10));
+    // System.out.println(Arrays.deepToString(gerarMatrizQuadrada(10)));
   }
 }
