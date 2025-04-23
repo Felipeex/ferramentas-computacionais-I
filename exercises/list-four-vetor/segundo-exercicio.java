24558b23c8e60fc6836405d383570043927926b7
Felipe Lima Santos <felipeex@MacBook-Air-de-Felipe.local>
2025-04-22 22:11:44 -0300

    .

--- exercises/list-four-vetor/segundo-exercicio.java
+++ exercises/list-four-vetor/segundo-exercicio.java
@@ -1,4 +1,6 @@
+
 import java.util.Arrays;
+
 
 class SegundoExercicio {
  static int[][] fakeQM(int n) {
@@ -27,28 +29,68 @@ class SegundoExercicio {
         System.out.println(matriz[linha][coluna]);
   }
 
-  static void verificaQM(int matriz[][]) {
+  static boolean verificaQM(int matriz[][]) {
     int somaElementosLinha[] = new int[matriz.length], somaElementosColuna[] = new int[matriz.length];
+    int somaPrimeiraDiagonal = 0, somaSegundaDiagonal = 0;
 
+    // somaElementosLinha
     for(int linha = 0; linha < matriz.length; linha++) {
       for(int coluna = 0; coluna < matriz.length; coluna++) {
         somaElementosLinha[linha] += matriz[linha][coluna];
       }
     }
-
+    
+    // somaElementosColuna
     for(int coluna = 0; coluna < matriz.length; coluna++) {
       for(int linha = 0; linha < matriz.length; linha++) {
         somaElementosColuna[coluna] += matriz[linha][coluna];
       }
     }
 
-    System.out.println(Arrays.toString(somaElementosLinha));
-    System.out.println(Arrays.toString(somaElementosColuna));
+    // somaPrimeiraDiagonal
+    for(int linha = matriz.length - 1, coluna = 0; linha >= 0 && coluna <= matriz.length; linha--, coluna++) {
+      somaPrimeiraDiagonal += matriz[linha][coluna];
+    }
+
+    // somaSegundaDiagonal
+    for (int linha = 0, coluna = 0; linha < matriz.length; linha++, coluna++) {
+      somaSegundaDiagonal += matriz[linha][coluna];
+    }
+
+    // verifica somaElementosLinha
+    for(int i = 0; i < somaElementosLinha.length; i++) {
+      if (somaElementosLinha[0] != somaElementosLinha[i]) return false;
+    }
+
+    // verifica somaElementosColuna
+    for(int i = 0; i < somaElementosColuna.length; i++) {
+      if (somaElementosColuna[0] != somaElementosColuna[i]) return false;
+    }
+
+    // verifica diagonal
+    if (somaPrimeiraDiagonal != somaSegundaDiagonal) {
+      return false;
+    }
+
+    return true;
   }
+
+  static void gerarAteAchar() {
+    boolean isQM;
+    int matriz[][];
+
+    do {
+      matriz = gerarMatrizQuadrada(3);
+
+      isQM = verificaQM(matriz);
+      System.out.println(Arrays.deepToString(matriz));
+    } while(!isQM);
+  } 
   
   public static void main(String[] args) {
-    // exibeMatrizQuadrada(gerarMatrizQuadrada(10));
-    verificaQM(gerarMatrizQuadrada(10));
-    // System.out.println(Arrays.deepToString(gerarMatrizQuadrada(10)));
+    int matriz[][] = gerarMatrizQuadrada(3);
+
+    exibeMatrizQuadrada(matriz);
+    System.out.println("É um quadro mágico? " + (verificaQM(matriz) ? "Sim" : "Não"));
   }
 }
